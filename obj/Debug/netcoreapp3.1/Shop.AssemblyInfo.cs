d6780141c5ff9b2bd59cfcd194bc7348cc18445c
Carlos <carlosz.eduardo1402@gmail.com>
2025-01-23 23:49:56 -0300

    updating and formatting code

--- obj/Debug/netcoreapp3.1/Shop.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/Shop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e697fed202fb007d25602a463341ce7195f394fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a69b6f31833438d0cda3a0b4fabc7ac7b19f7e32")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
